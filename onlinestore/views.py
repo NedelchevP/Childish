4db2846d9c4ff9535b46ab579ee38470751c3847
nedelchev <pavel@bytekmarketing.it>
2022-08-08 00:03:15 +0300

    cleanup + api/stats dev

--- onlinestore/views.py
+++ onlinestore/views.py
@@ -1,85 +1,59 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.viewsets import GenericViewSet
 
 from onlinestore.models import Product, Orders
 from onlinestore.pagination import StandardResultsSetPagination
-from onlinestore.serializers import ProductSerializer, OrdersSerializer
+from onlinestore.serializers import ProductSerializer, OrdersSerializer, ApiStatsSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    # filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
-    # filterset_class = BrandFilter
+    permission_classes = []
     ordering_fields = ['title', 'price']
     pagination_class = StandardResultsSetPagination
 
-    # def create(self, request, *args, **kwargs):
-    #     """Override CREATE"""
-    #     return super(ProductViewSet, self).create(request, *args, **kwargs)
-    #
-    # def retrieve(self, request, *args, **kwargs):
-    #     """Override RETRIEVE"""
-    #     return super(ProductViewSet, self).retrieve(request, *args, **kwargs)
-    #
-    # def update(self, request, *args, **kwargs):
-    #     """Override UPDATE"""
-    #     return super(ProductViewSet, self).update(request, *args, **kwargs)
-    #
-    # def destroy(self, request, *args, **kwargs):
-    #     """Override DESTROY"""
-    #     return super(ProductViewSet, self).destroy(request, *args, **kwargs)
-    #
-    # def list(self, request, *args, **kwargs):
-    #     """Override LIST and adds django-filter functionality ??"""
-    #     return super(ProductViewSet, self).list(request, *args, **kwargs)
 
 class OrdersViewSet(viewsets.ModelViewSet):
     queryset = Orders.objects.all()
     serializer_class = OrdersSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    # filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
-    # filterset_class = BrandFilter
+    permission_classes = []
     ordering_fields = ['date']
     pagination_class = StandardResultsSetPagination
 
-    # def create(self, request, *args, **kwargs):
-    #     """Override CREATE"""
-    #     return super(OrdersViewSet, self).create(request, *args, **kwargs)
-    #
-    # def retrieve(self, request, *args, **kwargs):
-    #     """Override RETRIEVE"""
-    #     return super(OrdersViewSet, self).retrieve(request, *args, **kwargs)
-    #
-    # def update(self, request, *args, **kwargs):
-    #     """Override UPDATE"""
-    #     return super(OrdersViewSet, self).update(request, *args, **kwargs)
-    #
-    # def destroy(self, request, *args, **kwargs):
-    #     """Override DESTROY"""
-    #     return super(OrdersViewSet, self).destroy(request, *args, **kwargs)
-    #
-    # def list(self, request, *args, **kwargs):
-    #     """Override LIST and adds django-filter functionality ??"""
-    #     return super(OrdersViewSet, self).list(request, *args, **kwargs)
 
-    """
-    @action(detail=True, methods=['post'])
-    def remove_user(self, request, pk):
-        brand_id = pk
-        user_id = request.data.get('user_id') if 'user_id' in request.data else None
-        if not (brand_id and brand_id.isnumeric()) or not (user_id and user_id.isnumeric()):
-            return Response({"error": 'brand_id(int) AND user_id(int) are required!'},
-                            status=status.HTTP_400_BAD_REQUEST)
-        brand = Brand.objects.get(pk=brand_id)
-        user = User.objects.get(pk=user_id)
-        brand.users.remove(user)
-        return Response({"message": f"removed user {user.email}({user.id}) from the brand {brand.name}({brand.id})!"})
+class ApiStats(GenericViewSet):
+    serializer_class = ApiStatsSerializer
+    queryset = Orders.objects.all()
+    permission_classes = []
 
-    @action(detail=False, methods=['get'])
-    def get_brand_categories(self, request):
-        return Response(dict((x.id, x.name) for x in BrandCategory.objects.all()))
+    @action(detail=False, methods=['post'])
+    def stats(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            from django.db.models import Count, Sum
+            from django.db.models.functions import ExtractMonth
 
-    """
+            month_queryset = self.queryset.values(
+                'id',
+                'date',
+                'products'
+            ).annotate(
+                month=ExtractMonth('date')
+            )
+
+            if serializer.validated_data['metric'] == 'price':
+                final_queryset = month_queryset.values('month').annotate(
+                    value=Sum('products__price')
+                ).order_by()
+            elif serializer.validated_data['metric'] == 'count':
+                final_queryset = month_queryset.values('month').annotate(
+                    value=Count('products'),
+                ).order_by()
+            return Response(final_queryset)
+        else:
+            print("no")
+
+        return Response()
