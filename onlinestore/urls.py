d856b85abc8172d985e17d41d44c483729ae2a7d
nedelchev <pavel@bytekmarketing.it>
2022-08-08 00:02:22 +0300

    orders routing + api/stats path

--- onlinestore/urls.py
+++ onlinestore/urls.py
@@ -6,10 +6,10 @@ from rest_framework.routers import DefaultRouter
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'product', views.ProductViewSet, basename="products")
-
+router.register(r'order', views.OrdersViewSet, basename="orders")
 
 
 urlpatterns = [
     path('', include(router.urls)),
-
+    path('api/stats/', views.ApiStats.as_view({'post': 'stats'}))
 ]
